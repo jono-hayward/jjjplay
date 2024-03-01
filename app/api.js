487276f5fab92bea380654c9b91073aa90f30ed0
Jono Hayward <gabs-crests0s@icloud.com>
2024-03-01 15:20:23 +1100

    Refactor to get around service slowness issues
    Now finds the most recent post and searches all tracks since then.

---
+++ app/api.js
@@ -0,0 +1,56 @@
+import 'dotenv/config';
+
+import pkg from '@atproto/api';
+const { BskyAgent } = pkg;
+
+const config = {
+  bsky_handle:   process.env.BSKY_HANDLE,
+  bsky_username: process.env.BSKY_USERNAME,
+  bsky_password: process.env.BSKY_PASSWORD,
+  timezone: process.env.TIMEZONE,
+};
+const timeOptions = {
+  timeStyle: 'short',
+  timeZone: config.timezone,
+};
+
+
+// Begin talking to Bluesky
+console.log( '🪵 Logging in to Bluesky' );
+const agent = new BskyAgent({ service: "https://bsky.social" });
+await agent.login({
+  identifier: config.bsky_username,
+  password: config.bsky_password,
+});
+
+// Get latest post date
+console.log( '🔍 Finding the time of the most recent post' );
+let latest;
+const feed = await agent.getAuthorFeed({
+  actor: config.bsky_handle,
+  limit: 1,
+});
+
+if ( feed && feed.data ) {
+  latest = new Date( feed.data.cursor );
+} else {
+  const now = new Date();
+  now.setMinutes(now.getMinutes() - 10);
+  latest = now;
+}
+console.log( `⌚️ Latest post was at ${latest.toLocaleTimeString( 'en-AU', timeOptions )}` );
+
+
+// const API = `https://music.abcradio.net.au/api/v1/plays/triplej/search.json?tz=${config.timezone}`;
+const stamp = latest.toISOString().replace('Z', '+00:00:00');
+const API = `https://music.abcradio.net.au/api/v1/plays/search.json?station=triplej&order=asc&tz=${config.timezone}&from=${encodeURIComponent(stamp)}`;
+console.log( 'Querying', API );
+
+const scrape = async () => fetch(API).then( response => response.json() );
+
+const tracks = await scrape();
+
+console.log( tracks );
+for ( const song of tracks.items ) {
+  console.log( song );
+}
--- app/scrape.js
+++ app/scrape.js
@@ -13,9 +13,10 @@ import pkg from '@atproto/api';
 const { BskyAgent } = pkg;
 
 const config = {
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  timezone: process.env.TIMEZONE,
+  bsky_handle:    process.env.BSKY_HANDLE,
+  bsky_username:  process.env.BSKY_USERNAME,
+  bsky_password:  process.env.BSKY_PASSWORD,
+  timezone:       process.env.TIMEZONE,
 };
 
 const timeOptions = {
@@ -23,133 +24,170 @@ const timeOptions = {
   timeZone: config.timezone,
 };
 
-
-const API = `https://music.abcradio.net.au/api/v1/plays/triplej/now.json?tz=${config.timezone}`;
-
-const scrape = async () => fetch(API).then( response => response.json() );
-const playing = await scrape();
-
-
-// Bail out now if there's nothing playing
-if ( !playing.now || !playing.now.recording ) {
-  console.error( 'No song currently playing' );
-  process.exit(0);
-}
-
-const song = parse( playing.now );
-const songDate = new Date( song.started );
-
-console.log( '💿 Now playing', song );
-
-// Begin our bluesky post
-const postObject = {
-  langs: ['en-AU', 'en'],
-  createdAt: songDate.toISOString(),
-};
-
-const lines = [
-  `${clockEmoji( config.timezone, song.started )} ${songDate.toLocaleTimeString( 'en-AU', timeOptions )}`,
-  ``,
-  `🎵 ${song.title}`,
-  `🧑‍🎤 ${song.artist}`,  
-];
-
-if ( song.album !== song.title ) {
-  lines.push( `💿 ${song.album}` );
-}
-
-// Search the music streaming services for our song
-const streamingLinks = [];
-console.log( '🔍 Searching streaming services...' );
-
-const appleMusic = await searchAppleMusic( song );
-appleMusic && streamingLinks.push({
-  service: 'Apple Music',
-  url: appleMusic,
-}) && console.log( '✅ Found song on Apple Music' );
-
-const spotify = await searchSpotify( song );
-spotify && streamingLinks.push({
-  service: 'Spotify',
-  url: spotify,
-}) && console.log( '✅ Found song on Spotify' );
-
-const yt = await searchYouTube( song );
-yt && streamingLinks.push({
-  service: 'YouTube Music',
-  url: yt,
-}) && console.log( '✅ Found song on YouTube Music' );
-
-// Add found streaming services to the post
-streamingLinks.length && lines.push(
-  ``,
-  `🎧 ${streamingLinks.map( service => service.service ).join(' / ')}`
-);
-
-
 // Begin talking to Bluesky
 console.log( '🪵 Logging in to Bluesky' );
 const agent = new BskyAgent({ service: "https://bsky.social" });
 await agent.login({
-  identifier: config.username,
-  password: config.password,
+  identifier: config.bsky_username,
+  password: config.bsky_password,
 });
 
+// Get latest post date
+console.log( '🔍 Finding the time of the most recent post' );
+let latest;
+const feed = await agent.getAuthorFeed({
+  actor: config.bsky_handle,
+  limit: 1,
+});
 
-// Put the post together
-const rt = lines.join('\n');
+if ( feed && feed.data ) {
+  latest = new Date( feed.data.cursor );
+} else {
+  const now = new Date();
+  now.setMinutes(now.getMinutes() - 10);
+  latest = now;
+}
+console.log( `⌚️ Latest post was at ${latest.toLocaleTimeString( 'en-AU', timeOptions )}` );
 
-// Add the link facets to the post
-for ( const stream of streamingLinks ) {
+const params = new URLSearchParams( {
+  station: 'triplej',
+  order: 'desc', // We want them in descending order to always get the latest, even if for some reason there's more results than our limit
+  tz: config.timezone,
+  from: latest.toISOString().replace('Z', '+00:00:00'),
+  limit: 10,
+} );
 
-  // Find the service name we added earlier
-  const { start, end } = findByteRange( rt, stream.service );
-  
-  if ( !postObject.facets ) {
-    postObject.facets = [];
+const API = `https://music.abcradio.net.au/api/v1/plays/search.json?${params.toString()}`;
+console.log( 'Querying', API );
+
+const scrape = async () => fetch(API).then( response => response.json() );
+
+const tracks = await scrape();
+
+/**
+ * Sort the items into ascending order, so we post from oldest to most recent.
+ * Technically since we're setting the createdAt attribute to the played time anyway,
+ * this shouldn't matter. But it feels neater to do it this way?
+ */
+
+if ( !tracks.total ) {
+  console.log( '🚦 No new plays since last post' );
+  process.exit(0);
+}
+
+tracks.items.sort((a, b) => new Date(a.played_time) - new Date(b.played_time));
+
+/** Iterate through tracks */
+for ( const track of tracks.items ) {
+
+  const song = parse( track );
+  const songDate = new Date( song.started );
+
+  console.log( ' ' );
+  console.log( `🎵 Processing "${song.title} by ${song.artist}, played at ${songDate.toLocaleTimeString( 'en-AU', timeOptions )}` );
+
+  // Begin our bluesky post
+  const postObject = {
+    langs: ['en-AU', 'en'],
+    createdAt: songDate.toISOString(),
+  };
+
+  const lines = [
+    `${clockEmoji( config.timezone, song.started )} ${songDate.toLocaleTimeString( 'en-AU', timeOptions )}`,
+    ``,
+    `🎵 ${song.title}`,
+    `🧑‍🎤 ${song.artist}`,  
+  ];
+
+  if ( song.album !== song.title ) {
+    lines.push( `💿 ${song.album}` );
   }
 
-  postObject.facets.push({
-    index: {
-      byteStart: start,
-      byteEnd: end
-    },
-    features: [{
-      $type: 'app.bsky.richtext.facet#link',
-      uri: stream.url
-    }]
-  });
+  // Search the music streaming services for our song
+  const streamingLinks = [];
+  console.log( '🔍 Searching streaming services...' );
+
+  const appleMusic = await searchAppleMusic( song );
+  appleMusic && streamingLinks.push({
+    service: 'Apple Music',
+    url: appleMusic,
+  }) && console.log( '✅ Found song on Apple Music' );
+
+  const spotify = await searchSpotify( song );
+  spotify && streamingLinks.push({
+    service: 'Spotify',
+    url: spotify,
+  }) && console.log( '✅ Found song on Spotify' );
+
+  const yt = await searchYouTube( song );
+  yt && streamingLinks.push({
+    service: 'YouTube Music',
+    url: yt,
+  }) && console.log( '✅ Found song on YouTube Music' );
+
+  // Add found streaming services to the post
+  streamingLinks.length && lines.push(
+    ``,
+    `🎧 ${streamingLinks.map( service => service.service ).join(' / ')}`
+  );
+
+
+  // Put the post together
+  const rt = lines.join('\n');
+
+  // Add the link facets to the post
+  for ( const stream of streamingLinks ) {
+
+    // Find the service name we added earlier
+    const { start, end } = findByteRange( rt, stream.service );
+    
+    if ( !postObject.facets ) {
+      postObject.facets = [];
+    }
+
+    postObject.facets.push({
+      index: {
+        byteStart: start,
+        byteEnd: end
+      },
+      features: [{
+        $type: 'app.bsky.richtext.facet#link',
+        uri: stream.url
+      }]
+    });
+  }
+
+  postObject.text = rt;
+
+  if ( song.artwork ) {
+    console.log( ' ' );
+
+    console.log( '🖼️ Grabbing artwork' );
+    const response = await fetch( song.artwork );
+    const buffer = await response.arrayBuffer();
+
+    console.log( '⬆️ Uploading artwork to Bluesky...' );
+    const { data } = await agent.uploadBlob( new Uint8Array( buffer ), { encoding: 'image/jpeg' } );
+    console.log( '✅ Uploaded!' );
+
+    postObject.embed = {
+      $type: 'app.bsky.embed.images',
+      images: [{
+        alt: `Album artwork for "${song.album}" by ${song.artist}`,
+        image: data.blob,
+        aspectRatio: {
+          width: 1,
+          height: 1,
+        }
+      }]
+    };
+    console.log( ' ' );
+  }
+
+  console.log( '🚀 Posting to Bluesky', postObject );
+  await agent.post( postObject );
+  console.log( '✅ Done!' );
+
 }
-
-postObject.text = rt;
-
-if ( song.artwork ) {
-  console.log( '' );
-
-  console.log( '🖼️ Grabbing artwork' );
-  const response = await fetch( song.artwork );
-  const buffer = await response.arrayBuffer();
-
-  console.log( '⬆️ Uploading artwork to Bluesky...' );
-  const { data } = await agent.uploadBlob( new Uint8Array( buffer ), { encoding: 'image/jpeg' } );
-  console.log( '✅ Uploaded!' );
-
-  postObject.embed = {
-    $type: 'app.bsky.embed.images',
-    images: [{
-      alt: `Album artwork for "${song.album}" by ${song.artist}`,
-      image: data.blob,
-      aspectRatio: {
-        width: 1,
-        height: 1,
-      }
-    }]
-  };
-  console.log( '' );
-}
-
-console.log( '🚀 Posting to Bluesky', postObject );
-await agent.post( postObject );
-console.log( '✅ Done!' );
 
 process.exit(0);
