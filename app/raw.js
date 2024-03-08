10a06962fc2fed3feff6fccc6d5c7d083661d1e2
Jono Hayward <gabs-crests0s@icloud.com>
2024-03-08 16:15:31 +1100

    Add Double J workflow

--- app/raw.js
+++ app/raw.js
@@ -1,13 +1,18 @@
 import 'dotenv/config';
 
+const config = {
+  station:        process.env.STATION,
+  timezone:       process.env.TIMEZONE,
+};
+
 const now = new Date();
 now.setHours( now.getHours() - 8 );
 now.setMinutes( now.getMinutes() - 10 );
 
 const params = new URLSearchParams( {
-  station: 'triplej',
+  station: config.station,
   order: 'desc', // We want them in descending order to always get the latest, even if for some reason there's more results than our limit
-  tz: 'Australia/Perth',
+  tz: config.timezone,
   limit: 20,
 } );
 
